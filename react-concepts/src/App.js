b7d647d3b61aef849543220eec04e3d27e558b70
hstrada <h.strada@hotmail.com>
2021-05-17 20:04:25 -0300

    create useFetch hook

--- react-concepts/src/App.js
+++ react-concepts/src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
 
+const tahoe_peaks = [
+  { name: 'Freel Peak', elevation: 10891 },
+  { name: 'Monument Peak', elevation: 10067 },
+  { name: 'Pyramid Peak', elevation: 9983 },
+  { name: 'Mt. Tallac', elevation: 9735 },
+];
+
+function List({ data = [], renderItem, renderEmpty }) {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item, i) => (
+        <li key={i}>{renderItem(item)}</li>
+      ))}
+    </ul>
+  );
+}
+
 function App() {
   return (
-    <div>
-      <p>Hello</p>
-    </div>
+    <List
+      data={tahoe_peaks}
+      renderEmpty={<p>This list is empty</p>}
+      renderItem={(item) => (
+        <>
+          {item.name} - {item.elevation.toLocaleString()}ft
+        </>
+      )}
+    />
   );
 }
 
